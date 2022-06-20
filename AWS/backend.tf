ff865dcf473306794074fce4c863f532ef7ab2c1
andreohirata <105748364+andreohirata@users.noreply.github.com>
2022-06-20 17:58:45 -0300

    Update backend - Terraform Cloud

--- AWS/backend.tf
+++ AWS/backend.tf
@@ -16,3 +16,13 @@
 #    }
 #  }
 #}
+
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "ccoe-ttech"
+    workspaces {
+      name = "atlantis-aws"
+    }
+  }
+}
