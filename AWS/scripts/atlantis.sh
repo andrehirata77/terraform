8a433f89733859f47d9edc646f1554e86dd816d4
andreohirata <105748364+andreohirata@users.noreply.github.com>
2022-06-21 14:27:00 -0300

    Uodate atlantis.sh and user_data.sh

--- AWS/scripts/atlantis.sh
+++ AWS/scripts/atlantis.sh
@@ -10,3 +10,20 @@ sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install terraform
+
+
+#!/bin/bash
+# wget https://github.com/runatlantis/atlantis/releases/download/v0.19.3/atlantis_linux_amd64.zip
+# sudo apt -y install unzip
+# unzip atlantis_linux_amd64
+# sudo apt update
+# sudo apt -y install software-properties-common
+# sudo add-apt-repository --yes --update ppa:ansible/ansible
+# sudo apt -y install ansible
+# sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+# curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+# sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+# sudo apt-get update && sudo apt-get install terraform
+# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# sudo apt install python3-pip
+# pip install boto3
--- AWS/scripts/user_data.sh
+++ AWS/scripts/user_data.sh
@@ -12,19 +12,3 @@ echo '<h1><center>Virtual Machine generated with Terraform - Git - Atlantis</cen
 echo '<h1><center>Telefonica Tech - CCoE - Arquitetura e Engenharia</center></h1>' >> index.html
 echo "<h1><center>Servidor: $host</center></h1>" >> index.html
 sudo mv index.html /usr/share/nginx/html/
-
-#!/bin/bash
-# wget https://github.com/runatlantis/atlantis/releases/download/v0.19.3/atlantis_linux_amd64.zip
-# sudo apt -y install unzip
-# unzip atlantis_linux_amd64
-# sudo apt update
-# sudo apt -y install software-properties-common
-# sudo add-apt-repository --yes --update ppa:ansible/ansible
-# sudo apt -y install ansible
-# sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
-# curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-# sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-# sudo apt-get update && sudo apt-get install terraform
-# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-# sudo apt install python3-pip
-# pip install boto3
